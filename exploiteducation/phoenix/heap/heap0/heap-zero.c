2dfa202ea0948a32b52b2c06738e3d5f0d770af4
docfate111 <tdwilliamsiv@gmail.com>
2020-06-20 15:17:32 -0700

    house of chaos malloc() error

---
+++ exploiteducation/phoenix/heap/heap0/heap-zero.c
@@ -0,0 +1,61 @@
+/*
+ * phoenix/heap-zero, by https://exploit.education
+ *
+ * Can you hijack flow control, and execute the winner function?
+ *
+ * Why do C programmers make good Buddhists?
+ * Because they're not object orientated.
+ */
+
+#include <err.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define BANNER \
+  "Welcome to " LEVELNAME ", brought to you by https://exploit.education"
+
+struct data {
+  char name[64];
+};
+
+struct fp {
+  void (*fp)();
+  char __pad[64 - sizeof(unsigned long)];
+};
+
+void winner() {
+  printf("Congratulations, you have passed this level\n");
+}
+
+void nowinner() {
+  printf(
+      "level has not been passed - function pointer has not been "
+      "overwritten\n");
+}
+
+int main(int argc, char **argv) {
+  struct data *d;
+  struct fp *f;
+
+  printf("%s\n", BANNER);
+
+  if (argc < 2) {
+    printf("Please specify an argument to copy :-)\n");
+    exit(1);
+  }
+
+  d = malloc(sizeof(struct data));
+  f = malloc(sizeof(struct fp));
+  f->fp = nowinner;
+
+  strcpy(d->name, argv[1]);
+
+  printf("data is at %p, fp is at %p, will be calling %p\n", d, f, f->fp);
+  fflush(stdout);
+
+  f->fp();
+
+  return 0;
+}
